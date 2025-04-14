6033fcf7097fd650a92fa1d7a2e8729812cac71a
auto-tube <clanceyhiggens@gmail.com>
2025-04-14 04:18:10 -0700

    minor ux fixes

--- Views/Converters.cs
+++ Views/Converters.cs
@@ -1,3 +1,4 @@
+using MaterialDesignThemes.Wpf; // Required for PackIconKind
 using System;
 using System.Globalization;
 using System.Windows.Data;
@@ -34,10 +35,30 @@ namespace AutoTubeWpf.Views // Keep namespace consistent with Views
         {
             if (value is bool isProcessing)
             {
-                // Using standard WPF Brushes
+                // Using standard WPF Brushes - Consider using MaterialDesign brushes later
                 return isProcessing ? Brushes.Red : Brushes.Green;
             }
             return Brushes.Green; // Default fallback
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException(); // One-way conversion
+        }
+    }
+
+    /// <summary>
+    /// Converts a boolean (IsPlayerPlaying) to the appropriate Material Design Icon Kind for Play/Pause.
+    /// </summary>
+    public class BoolToPlayPauseIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool isPlaying)
+            {
+                return isPlaying ? PackIconKind.Pause : PackIconKind.Play;
+            }
+            return PackIconKind.Play; // Default to Play icon
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
